291fdd03522fd8a1574146702ebf7c931c39012d
Daniel Jones <YOUR_ID@users.noreply.github.com>
2026-03-07 18:58:25 -0800

    "Smooth scroll added"

---
+++ smooth-scroll.js
@@ -0,0 +1,12 @@
+const lenis = new Lenis({
+    duration: 1.2,
+    easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+    touchMultiplier: 2,
+});
+
+function raf(time) {
+    lenis.raf(time);
+    requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
